8bb551504585bc96c108a4fc66bcdf1dae729522
Arthus <531151995@qq.com>
2023-02-14 11:30:44 +0800

    feat(fix):finally

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, HostListener, OnDestroy, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, HostListener, OnDestroy, ViewChild, ViewContainerRef } from '@angular/core';
 import { ScrollMotionHelper, DescriptionComponent } from '@lib';
 import { IScrollMotion, IScrollMotionConfig } from '@types';
 import mframe from 'mframe';
@@ -20,7 +20,9 @@ export class BannerComponent implements OnDestroy, IScrollMotion, AfterViewInit 
         conH: 0,
         titleH: 0,
         y0: 0,
-        y1: 0
+        y1: 0,
+        translateY: '60%',
+        scale: 0.8
     };
 
     @ViewChild('background') background: ElementRef;
@@ -34,8 +36,17 @@ export class BannerComponent implements OnDestroy, IScrollMotion, AfterViewInit 
         this.resize();
     }
 
-    constructor(private viewContainerRef: ViewContainerRef) {
+    constructor(
+        private viewContainerRef: ViewContainerRef,
+        private cd: ChangeDetectorRef
+    ) {
 
+    }
+
+    private forceUpdate() {
+        if (this.cd) {
+            this.cd.detectChanges();
+        }
     }
 
     ngOnDestroy() {
@@ -49,6 +60,7 @@ export class BannerComponent implements OnDestroy, IScrollMotion, AfterViewInit 
     }
 
     resize() {
+        let bodyW =  document.getElementsByTagName('body')[0].clientWidth;
         let background = this.background.nativeElement as HTMLElement;
         let w = background.clientWidth;
         let elementTitle = this.desc.viewContainerRef.element.nativeElement as HTMLElement;
@@ -56,47 +68,55 @@ export class BannerComponent implements OnDestroy, IScrollMotion, AfterViewInit 
         let bh = 673/1400*w>>0;
 
         this.res.titleH = titleH;
-        if(w>2560) {
-            this.res.conH = bh+titleH/2;
-            this.res.y0 = titleH*1;
-            this.res.y1 = titleH*0.8;
-        } else if(w>1960) {
-            this.res.conH = bh+titleH/2;
-            this.res.y0 = titleH*0.8;
-            this.res.y1 = titleH*0.7;
-        } else if(w>1440) {
-            this.res.conH = bh+titleH/2;
-            this.res.y0 = titleH*0.7;
-            this.res.y1 = titleH*0.6;
-        } else if(w>1080) {
-            this.res.conH = bh+titleH/2;
-            this.res.y0 = titleH*0.6;
-            this.res.y1 = titleH*0.5;
-        } else {
+        this.res.translateY = '60%';
+        this.res.conH = bh+titleH/2;
+        this.res.scale = '0.8';
+
+        if(bodyW>1960) {
+            this.res.y0 = '-20%';
+            this.res.y1 = '-30%';
+        } else if(bodyW>1440) {
+            this.res.y0 = '-23%';
+            this.res.y1 = '-33%';
+        } else if(bodyW>1080) {
+            this.res.y0 = '-30%';
+            this.res.y1 = '-40%';
+            this.res.translateY = '40%';
+        } else if(bodyW>560) {
+            this.res.y0 = '-40%';
+            this.res.y1 = '-60%';
+            this.res.translateY = '15%';
+            this.res.scale = '0.9';
+        }else {
+            this.res.y0 = '-10%';
+            this.res.y1 = '-30%';
+            this.res.scale = '0.92';
             this.res.conH = bh+titleH;
-            this.res.y0 = 0;
-            this.res.y1 = 0;
+            this.res.translateY = '20%';
         }
        
         background.style.height = bh + 'px';
-        background.style.top = this.res.y0  + 'px';
         this.viewContainerRef.element.nativeElement.style.height = this.res.conH +'px';
-
         this.initMotion();
+        this.forceUpdate();
     }
 
     initMotion() {
         this.scrollMotionConfig = ScrollMotionHelper.getDomViewRange(this.viewContainerRef.element.nativeElement);
-        if(!this.scrollMotion) {
-            const time = this.scrollMotionConfig.hide.start - this.scrollMotionConfig.show.end;
-            this.scrollMotion =  mframe([{
-                dom: this.background.nativeElement,
-                frames: [
-                    { css: { transform: 'scale(1.0)', top: this.res.y0+'px' }, time:0},
-                    { css: { transform: 'scale(0.74)' , top: this.res.y1+'px' }, time: time, tween: 'easeInOut'},
-                ]
-            }]);
-        };
+        const time = this.scrollMotionConfig.hide.start - this.scrollMotionConfig.show.end;
+        this.scrollMotion =  mframe([{
+            dom: this.background.nativeElement,
+            frames: [
+                { css: { transform: `scale(1.0) translateY(${this.res.y0})` }, time:0},
+                { css: { transform: `scale(${this.res.scale}) translateY(${this.res.y1})` }, time: time, tween: 'easeInOut'},
+            ]
+        },{
+            dom: this.desc.viewContainerRef.element.nativeElement,
+            frames: [
+                { css: { transform: 'translateY(0%)'}, time:0},
+                { css: { transform: `translateY(${this.res.translateY})`}, time: time, tween: 'easeInOut'},
+            ]
+        }]);
         
         this.checkScroll();
     }
